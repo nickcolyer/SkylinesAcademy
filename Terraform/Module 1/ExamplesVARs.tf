c71e6ba4937e8aac4ac1c064376e768ec371ca80
Nick Colyer <nick.colyer@thinkahead.com>
2018-07-09 13:11:34 -0500

    Varaibles file update

--- Terraform/Module 1/ExamplesVARs.tf
+++ Terraform/Module 1/ExamplesVARs.tf
@@ -1,6 +1,6 @@
-provider "azurerm" {
-  subscription_id = "11111111-1111-1111-1111-111111111"
-  client_id       = "11111111-1111-1111-1111-111111111"
-  client_secret   = "11111111-1111-1111-1111-111111111"
-  tenant_id       = "11111111-1111-1111-1111-111111111"
-}
+#Rename file to terraform.tfvars and update the variables below for your environment.
+#subscription_id = "11111111-1111-1111-1111-111111111"
+#client_id = "11111111-1111-1111-1111-111111111"
+#client_secret = "11111111-1111-1111-1111-111111111"
+#tenant_id = "11111111-1111-1111-1111-111111111"
+
