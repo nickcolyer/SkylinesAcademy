790a3716edd199b64baa29d3d2ca10eaaf0e0e28
jamiejackson <jamiejaxon@gmail.com>
2018-10-28 14:30:40 -0400

    don't version .terraform dirs; tidy up .gitignore

--- Terraform/Module 7b/.terraform/modules/bc9999c9687b9722cd2e0ca00c88f379/Azure-terraform-azurerm-compute-627c6c2/test/fixture/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "location" {}
-variable "ssh_key" {}
-variable "resource_group_name" {}
-variable "vm_os_simple_1" {}
-variable "vm_os_simple_2" {}
-variable "admin_username" {}
-variable "admin_password" {}
